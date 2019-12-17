ecc9af1c8801896b57bfaccc148e68e184a78f1b
Zhuanle <1847496393@qq.com>
2019-12-17 17:58:39 +0800

    mavenprojuct1.2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,12 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            System.out.println( "Hello World!" );
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
 
+        System.out.println( "Hello World!" );
     }
 }
